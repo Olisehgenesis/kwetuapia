a30448c92c5faf8f8a99f2ac04325418bc31fefb
Oliseh genesis <mwijusyaoliseh1@gmail.com>
2021-07-20 20:33:58 +0300

    12

---
+++ app.py
@@ -0,0 +1,71 @@
+"""Flask App Project."""
+
+from flask import Flask, jsonify
+import json
+import pymongo
+from flask import request
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    json_data = {'Hello': 'World!'}
+    return jsonify(json_data)
+
+
+
+#connecting database
+client = pymongo.MongoClient("mongodb+srv://admin:admin@kwetu.ig6hf.mongodb.net/users?retryWrites=true&w=majority")
+db = client['users']
+collection = db['kwetu']
+
+
+@app.route('/login', methods=['POST'])
+def checkuser():
+    body = request.get_json()
+    email = body.get('email')
+    password = body.get('password')
+    if collection.count_documents({"email": email , "password" : password}) == 1:
+        return jsonify(
+            success= 1 ,
+            data = {'email': email,
+            'password' : password}
+        )
+    else:
+        return jsonify(
+            success= 0 ,
+            error_message = "invalid creditials")
+
+@app.route('/signup', methods=['POST'])
+def Createuser():
+    data = request.get_json()
+    email=data.get('email')
+    password = data.get('password')
+    phone= data.get('phone')
+    district = data.get('district')
+    user ={
+        "email" : email,
+        "password": password,
+        "phone": phone,
+        "district": district
+    }
+    if collection.count_documents({"email": email}) < 1:
+        collection.insert_one(user)
+        return jsonify(
+        success = 1,
+        message = "account successfully created"
+        )
+    else:
+        return jsonify(sucess = 0,
+        message = "email already in use"
+        )
+
+    
+if __name__ == '__main__':
+    app.run()
+
+
+
+if __name__ == '__main__':
+    app.run()
